afe9f1f486ffaf822de456851e4c22de923a7340
Adrian Mark Dalipe <50126402+adrnmrk@users.noreply.github.com>
2024-01-28 16:08:40 -0500

    updated button styles and colors

--- MAPD721A1AdrianDalipe/app/src/main/java/com/amd/mapd721_a1_adriandalipe/MainActivity.kt
+++ MAPD721A1AdrianDalipe/app/src/main/java/com/amd/mapd721_a1_adriandalipe/MainActivity.kt
@@ -3,6 +3,7 @@ package com.amd.mapd721_a1_adriandalipe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,8 +14,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,6 +30,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -69,21 +73,21 @@ fun MainScreen() {
     var email by remember { mutableStateOf("") }
     var userId by remember { mutableStateOf("") }
 
-    Column(modifier = Modifier.fillMaxSize()){
-       Text(
-           modifier = Modifier
-               .padding(16.dp, top = 30.dp),
-           text = "Username",
-           color = androidx.compose.ui.graphics.Color.Gray,
-           fontSize = 12.sp
-       )
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            modifier = Modifier
+                .padding(16.dp, top = 30.dp),
+            text = "Username",
+            color = androidx.compose.ui.graphics.Color.Gray,
+            fontSize = 12.sp
+        )
         //email
         OutlinedTextField(
             modifier = Modifier
                 .padding(start = 16.dp, end = 16.dp)
                 .fillMaxWidth(),
             value = username,
-            onValueChange = {username = it},
+            onValueChange = { username = it },
         )
         // Email Field
         Text(
@@ -126,40 +130,63 @@ fun MainScreen() {
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             // Save Button
-            Button(
-                modifier = Modifier
-                    .weight(1f)
-                    .height(60.dp)
-                    .padding(start = 8.dp, end = 8.dp)
-                    .background(color = androidx.compose.ui.graphics.Color.Green),
-                onClick = {
-                    scope.launch {
-                        dataStore.saveDetails(username, email, userId)
-                        email = ""
-                        username = ""
-                        userId = ""
-                    }
-                }
-            ) {
-                Text(text = "Save", color = androidx.compose.ui.graphics.Color.White, fontSize = 18.sp)
-            }
+
 
             // Load Button
             Button(
                 modifier = Modifier
                     .weight(1f)
                     .height(60.dp)
-                    .padding(start = 8.dp, end = 8.dp)
-                    .background(color = androidx.compose.ui.graphics.Color.Blue),
+                    .padding(start = 8.dp, end = 8.dp),
+
                 onClick = {
                     // Your action for the Load button
 
                     email = savedEmailState.value.orEmpty()
                     username = savedUsernameState.value.orEmpty()
                     userId = savedIDState.value.orEmpty()
-                }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.light_yellow)),
+                border = BorderStroke(
+                    width = 1.0.dp,
+                    color = androidx.compose.ui.graphics.Color.Yellow
+                ),
             ) {
-                Text(text = "Load", color = androidx.compose.ui.graphics.Color.White, fontSize = 18.sp)
+                Text(
+                    text = "Load",
+                    color = androidx.compose.ui.graphics.Color.Black,
+                    fontSize = 18.sp
+                )
+            }
+            //save button
+            Button(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(60.dp)
+                    .padding(start = 8.dp, end = 8.dp),
+                onClick = {
+
+                    scope.launch {
+                        dataStore.saveDetails(username, email, userId)
+                        email = ""
+                        username = ""
+                        userId = ""
+                    }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.light_green)),
+                border = BorderStroke(
+                    width = 1.0.dp,
+                    color = androidx.compose.ui.graphics.Color.Green,
+                ),
+
+                ) {
+                Text(
+                    text = "Save",
+                    color = androidx.compose.ui.graphics.Color.Black,
+                    fontSize = 18.sp
+                )
             }
 
             // Clear Button
@@ -167,8 +194,7 @@ fun MainScreen() {
                 modifier = Modifier
                     .weight(1f)
                     .height(60.dp)
-                    .padding(start = 8.dp, end = 8.dp)
-                    .background(color = androidx.compose.ui.graphics.Color.Red),
+                    .padding(start = 8.dp, end = 8.dp),
                 onClick = {
                     // Your action for the Clear button
                     scope.launch {
@@ -177,9 +203,19 @@ fun MainScreen() {
                         userId = ""
                         dataStore.clearDetails()
                     }
-                }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.light_red)),
+                border = BorderStroke(
+                    width = 1.0.dp,
+                    color = androidx.compose.ui.graphics.Color.Red
+                ),
             ) {
-                Text(text = "Clear", color = androidx.compose.ui.graphics.Color.White, fontSize = 18.sp)
+                Text(
+                    text = "Clear",
+                    color = androidx.compose.ui.graphics.Color.Black,
+                    fontSize = 18.sp
+                )
             }
         }
 
@@ -189,12 +225,11 @@ fun MainScreen() {
     }
 
 
-
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-        MainScreen()
+    MainScreen()
 
 }
